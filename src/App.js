df5bb6029924830ac91557109233bb9318d24ca9
Tristan <ricemaking69@gmail.com>
2023-11-10 10:05:54 -0600

    Stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import GradeCalculator from './GradeCalculator';
 import './App.css';
 
+// The app/website basically this
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Title">
+      <header className="Home-header">
+        <p>Calculate My Grades</p>
+        <a className='buttonTo' href="/grade-calculator"> Click Me</a>
       </header>
     </div>
   );
 }
 
-export default App;
+function Main() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/grade-calculator" element={<GradeCalculator />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Main;
---
+++ src/GradeCalculator.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+function GradeCalculator() {
+  // Rows/Arrays for each one.
+  const [assignmentName, setAssignmentName] = useState('');
+  const [assignmentWeight, setAssignmentWeight] = useState('');
+  const [grades, setGrades] = useState('');
+  const [assignmentItself, setAssignmentItself] = useState([]);
+
+  // Handles the change event.
+  const handleChange = (event, setStateFunction) => {
+    setStateFunction(event.target.value);
+  };
+
+  // Handles the submission.
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Create a new object representing the assignment
+    const newAssignment = {
+      name: assignmentName,
+      weight: assignmentWeight,
+      grade: grades,
+    };
+
+    // Add the new assignment to the array of assignments
+    setAssignmentItself((prevAssignments) => [...prevAssignments, newAssignment]);
+
+  };
+
+  return (
+    <div>
+      <p>Calculate My Grades</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="assignmentName"
+          value={assignmentName}
+          onChange={(e) => handleChange(e, setAssignmentName)}
+          placeholder="Assignment Name"
+        />
+        <input
+          type="text"
+          name="assignmentWeight"
+          value={assignmentWeight}
+          onChange={(e) => handleChange(e, setAssignmentWeight)}
+          placeholder="Weight"
+        />
+        <input
+          type="text"
+          name="grades"
+          value={grades}
+          onChange={(e) => handleChange(e, setGrades)}
+          placeholder="Grade"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default GradeCalculator;
