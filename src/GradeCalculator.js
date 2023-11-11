a7318cf751439bf1fca83d711de42cb17b160e3e
nt0307e <nt0307e@iastate.edu>
2023-11-10 18:19:58 -0600

    chore: added in a feature to put add more rows

--- src/GradeCalculator.js
+++ src/GradeCalculator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function GradeCalculator() {
   // Rows/Arrays for each one.
@@ -8,24 +8,72 @@ function GradeCalculator() {
   const [assignmentItself, setAssignmentItself] = useState([]);
 
   // Handles the change event.
-  const handleChange = (event, setStateFunction) => {
-    setStateFunction(event.target.value);
+  const handleChange = (event, field) => {
+    const [property, index] = field.split('_');
+    setAssignmentItself((prevAssignments) => {
+      const newAssignments = [...prevAssignments];
+      newAssignments[index] = {
+        ...newAssignments[index],
+        [property]: event.target.value,
+      };
+      return newAssignments;
+    });
   };
+
+  // Start off with 3 rows.
+  useEffect(() => {
+    setAssignmentItself([
+      { name: '', weight: '', grade: '' },
+      { name: '', weight: '', grade: '' },
+      { name: '', weight: '', grade: '' },
+    ]);
+  }, []);
+
+  const AssignmentRow = ({ assignment, index, handleChange }) => (
+    <div key={index}>
+      <input
+        type="text"
+        name={`assignmentName_${index}`}
+        value={assignment.name}
+        onChange={(e) => handleChange(e, `name_${index}`)}
+        placeholder="Assignment Name"
+      />
+      <input
+        type="text"
+        name={`assignmentWeight_${index}`}
+        value={assignment.weight}
+        onChange={(e) => handleChange(e, `weight_${index}`)}
+        placeholder="Weight"
+      />
+      <input
+        type="text"
+        name={`grades_${index}`}
+        value={assignment.grade}
+        onChange={(e) => handleChange(e, `grade_${index}`)}
+        placeholder="Grade"
+      />
+    </div>
+  );
 
   // Handles the submission.
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Create a new object representing the assignment
+    // Create a new object representing the assignment.
     const newAssignment = {
       name: assignmentName,
       weight: assignmentWeight,
       grade: grades,
     };
 
-    // Add the new assignment to the array of assignments
+    // Add the new assignment to the array of assignments.
     setAssignmentItself((prevAssignments) => [...prevAssignments, newAssignment]);
 
+  };
+
+  // Adds a new empty row.
+  const addRow = () => {
+        setAssignmentItself((prevAssignments => [...prevAssignments, {nmae: '', weight: '', grade: ''}]));
   };
 
   // The HTML return how it returns the contents of the page.
@@ -33,28 +81,18 @@ function GradeCalculator() {
     <div>
       <p>Calculate My Grades</p>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="assignmentName"
-          value={assignmentName}
-          onChange={(e) => handleChange(e, setAssignmentName)}
-          placeholder="Assignment Name"
-        />
-        <input
-          type="text"
-          name="assignmentWeight"
-          value={assignmentWeight}
-          onChange={(e) => handleChange(e, setAssignmentWeight)}
-          placeholder="Weight"
-        />
-        <input
-          type="text"
-          name="grades"
-          value={grades}
-          onChange={(e) => handleChange(e, setGrades)}
-          placeholder="Grade"
-        />
+        {assignmentItself.map((assignment, index) => (
+          <AssignmentRow
+            key={index}
+            assignment={assignment}
+            index={index}
+            handleChange={handleChange}
+          />
+        ))}
         <button type="submit">Submit</button>
+        <button type="button" onClick={addRow}>
+          Add Row
+        </button>
       </form>
     </div>
   );
